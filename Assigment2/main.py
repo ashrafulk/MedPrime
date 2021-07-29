6b085b8df2ee39d7ae08d4fcf6911d8f9150b80a
ashrafulk <ashrafulkhan4@gmail.com>
2021-07-30 00:16:20 +0530

    Create main.py

---
+++ Assigment2/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    l1=list(processed_text)
+    l2=['A','E',"I","O","U"]
+    cnt=0
+    for i in l1:
+        if i in l2:
+            cnt=cnt+1
+    r="{:.2f}".format(cnt/len(l1))
+    return r
